507f7395b9075d1fa8bff1578bc67d9f659759ed
spark <park1215@gmail.com>
2019-04-25 13:27:12 -0600

    initial commit
    test initial commit

--- demo.py
+++ demo.py
@@ -1,2 +1,26 @@
-print("Hello World!")
-print("New pycharm version is installed")
+import requests
+from bs4 import BeautifulSoup
+
+# response = requests.get("http://www.dowellcomputer.com/main.jsp")
+
+response = requests.get("https://www.viasat.com")
+
+html = response.text  # type of html is '<class 'str'>
+print(response.status_code)
+print(html)
+
+# Converting HTML source code to Python object
+soup = BeautifulSoup(html, "html.parser")
+# find an a tag in a td tags
+links = soup.select('td > a')
+
+print(len(links))
+
+for link in links:
+    if link.has_attri('href'):
+        if link.get('href').find('notice') !=-1:
+            print(link.text)
+        else:
+            print("None found")
+
+
