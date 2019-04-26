6a8fdfcee206ce99ecfd15ad51797b3a02a2d00b
spark <park1215@gmail.com>
2019-04-26 13:39:28 -0600

    initial commit
    Web crawler implementation

--- demo.py
+++ demo.py
@@ -1,6 +1,70 @@
 import requests
 from bs4 import BeautifulSoup
 
+class Conversation:
+    def __init__(self, question, answer):
+        self.question = question
+        self.answer = answer
+
+    def __str__(self):
+        return "question: " + self.question + "\nanswer: " + self.answer
+
+
+def get_subjects():
+    subjects = []
+
+    response = requests.get("https://basicenglishspeaking.com/daily-english-conversation-topics/")
+
+    html = response.text
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    divs = soup.findAll('div', {"class": "su-column-inner su-clearfix"})
+
+    for div in divs:
+        links = div.findAll('a')
+
+        for link in links:
+            subject = link.text
+            subjects.append(subject)
+
+    return subjects
+
+subjects = get_subjects()
+
+print("Total number of subjects : " + str(len(subjects)))
+
+print(subjects)
+
+conversations = []
+i = 1
+
+for sub in subjects:
+    print('(', i, '/', len(subjects), ')', sub)
+
+    response = requests.get("https://basicenglishspeaking.com/" + sub)
+
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+
+    qnas = soup.findAll('div', {"class": "sc_player_container1"})
+
+    if i == 3:
+        break
+
+    for qna in qnas:
+        if qnas.index(qna) % 2 ==0:
+            q = qna.next_sibling
+        else:
+            a=qna.next_sibling
+            c=Conversation(q, a)
+            conversations.append(c)
+    i = i+ 1
+
+print("Total conversations : ", len(conversations))
+
+for c in conversations:
+    print(str(c))
 
 # # response = requests.get("http://www.dowellcomputer.com/main.jsp")
 #
@@ -25,3 +89,5 @@ from bs4 import BeautifulSoup
 #             print("None found")
 
 # JUP-001 branch test"
+
+
