a9b006041bdb5014618914d21af8390df43cc457
spark <park1215@gmail.com>
2019-04-26 14:03:41 -0600

    Web crawler implementation
    used BeautifulSoup

--- demo.py
+++ demo.py
@@ -1,70 +1,92 @@
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
-class Conversation:
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
+MEMBER_DATA = {
+    'memberID' : 'a',
+    'memberPassword' : 'a'
+}
 
-    def __str__(self):
-        return "question: " + self.question + "\nanswer: " + self.answer
+with requests.Session() as s:
+    response = s.post('http://dowellcomputer.com/member/memberLoginAction.jsp', data=MEMBER_DATA)
+
+print(response.text)
+
+#logged in and moved to the next page
+
+response = s.get('http://dowellcomputer.com/member/memberUpdateForm.jsp?ID=a')
+soup = bs(response.text, 'html.parser')
+
+result = soup.findAll('input', {"name":"memberEmail"})
+
+print(result[0].get('value')) # print the member's email address
 
 
-def get_subjects():
-    subjects = []
+# class Conversation:
+#     def __init__(self, question, answer):
+#         self.question = question
+#         self.answer = answer
+#
+#     def __str__(self):
+#         return "question: " + self.question + "\nanswer: " + self.answer
+#
+#
+# def get_subjects():
+#     subjects = []
+#
+#     response = requests.get("https://basicenglishspeaking.com/daily-english-conversation-topics/")
+#
+#     html = response.text
+#
+#     soup = BeautifulSoup(html, 'html.parser')
+#
+#     divs = soup.findAll('div', {"class": "su-column-inner su-clearfix"})
+#
+#     for div in divs:
+#         links = div.findAll('a')
+#
+#         for link in links:
+#             subject = link.text
+#             subjects.append(subject)
+#
+#     return subjects
+#
+# subjects = get_subjects()
+#
+# print("Total number of subjects : " + str(len(subjects)))
+#
+# print(subjects)
+#
+# conversations = []
+# i = 1
+#
+# for sub in subjects:
+#     print('(', i, '/', len(subjects), ')', sub)
+#
+#     response = requests.get("https://basicenglishspeaking.com/" + sub)
+#
+#     html = response.text
+#     soup = BeautifulSoup(html, "html.parser")
+#
+#     qnas = soup.findAll('div', {"class": "sc_player_container1"})
+#
+#     if i == 3:
+#         break
+#
+#     for qna in qnas:
+#         if qnas.index(qna) % 2 ==0:
+#             q = qna.next_sibling
+#         else:
+#             a=qna.next_sibling
+#             c=Conversation(q, a)
+#             conversations.append(c)
+#     i = i+ 1
+#
+# print("Total conversations : ", len(conversations))
+#
+# for c in conversations:
+#     print(str(c))
 
-    response = requests.get("https://basicenglishspeaking.com/daily-english-conversation-topics/")
-
-    html = response.text
-
-    soup = BeautifulSoup(html, 'html.parser')
-
-    divs = soup.findAll('div', {"class": "su-column-inner su-clearfix"})
-
-    for div in divs:
-        links = div.findAll('a')
-
-        for link in links:
-            subject = link.text
-            subjects.append(subject)
-
-    return subjects
-
-subjects = get_subjects()
-
-print("Total number of subjects : " + str(len(subjects)))
-
-print(subjects)
-
-conversations = []
-i = 1
-
-for sub in subjects:
-    print('(', i, '/', len(subjects), ')', sub)
-
-    response = requests.get("https://basicenglishspeaking.com/" + sub)
-
-    html = response.text
-    soup = BeautifulSoup(html, "html.parser")
-
-    qnas = soup.findAll('div', {"class": "sc_player_container1"})
-
-    if i == 3:
-        break
-
-    for qna in qnas:
-        if qnas.index(qna) % 2 ==0:
-            q = qna.next_sibling
-        else:
-            a=qna.next_sibling
-            c=Conversation(q, a)
-            conversations.append(c)
-    i = i+ 1
-
-print("Total conversations : ", len(conversations))
-
-for c in conversations:
-    print(str(c))
+#*****************************
 
 
 # # response = requests.get("http://www.dowellcomputer.com/main.jsp")
